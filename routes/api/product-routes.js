f3c1ae516488ef035993033c7e9db6193d1cc768
dcrlsn <c4r150n@gmail.com>
2022-01-03 18:39:40 -0500

    Products get all

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,28 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.findAll(
+      {
+        include: [
+          {
+            model: Category,
+            attributes: ['category_name']
+          },
+          {
+            model: Tag,
+            attributes: ['tag_name']
+          }
+        ]
+      }
+    )
+    return res.json(products);
+  }
+  catch (err) {
+    console.log(err);
+    res.json(err);
+  }
 });
 
 // get one product
